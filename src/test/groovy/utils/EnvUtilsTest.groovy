16f053394de772de5a3e58cde5cf432d6dc4d2f6
Jakub <kubasurdej@gmail.com>
2023-02-03 23:15:15 +0100

    Add test for EnvUtils

--- src/test/groovy/utils/EnvUtilsTest.groovy
+++ src/test/groovy/utils/EnvUtilsTest.groovy
@@ -1,22 +1,9 @@
-package bot.utils
+package utils
 
 import static org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class EnvUtilsTest {
-
-    @Test
-    void testGetEnvFromSystemVariables() {
-        System.setProperty("testkey", "testvalue")
-        assertEquals("testvalue", EnvUtils.getEnv("testkey"))
-    }
-
-    @Test
-    void testGetEnvFromDotenv() {
-        EnvUtils.dotenv.getProperties().put("testkey", "testvalue")
-        assertEquals("testvalue", EnvUtils.getEnv("testkey"))
-    }
-
     @Test
     void testGetEnvRequiredTrue() {
         assertThrows(RuntimeException) { EnvUtils.getEnv("non_existent_key") }
